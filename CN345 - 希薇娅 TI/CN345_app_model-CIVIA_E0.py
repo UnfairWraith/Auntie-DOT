a1d2cc6d9be186ae3b7a4f7a3aa92fb2593553be
YuzuMin <69055192+YuzuMin@users.noreply.github.com>
2021-06-28 00:59:53 +0800

    Update CN345_app_model-CIVIA_E0.py

--- CN345 - 希薇娅 TI/CN345_app_model-CIVIA_E0.py
+++ CN345 - 希薇娅 TI/CN345_app_model-CIVIA_E0.py
@@ -7,13 +7,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from sklearn.metrics import confusion_matrix
-import efficientnet.keras as efn
 
 
 
+import time
+import datetime
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
 
 
 
+NUM_CLASSES = 10
+
+input_size = (600, 600)
+
+
+
+def create_model():
+    inputs = tf.keras.layers.Input(shape=(224, 224, 3))
+    efficientNet = tf.keras.applications.EfficientNetB0(weights="imagenet", include_top=False, input_tensor=inputs)
+    
+    #inputs = tf.keras.layers.Input(shape=(380, 380, 3))
+    #efficientNet = tf.keras.applications.EfficientNetB4(weights="imagenet", include_top=False, input_tensor=inputs)
+    
+    #inputs = tf.keras.layers.Input(shape=(600, 600, 3))
+    #efficientNet = tf.keras.applications.EfficientNetB7(weights="imagenet", include_top=False, input_tensor=inputs)
+    
+    #print("Number of layers in the base model: ", len(efficientNet.layers))
+    #print(efficientNet.summary())
+
+    #x = efficientNet.layers[-2].output
+
+    # Freeze the pretrained weights
+    efficientNet.trainable = False
+
+    # Rebuild top
+    x = tf.keras.layers.GlobalAveragePooling2D(name="avg_pool")(efficientNet.output)
+    x = tf.keras.layers.BatchNormalization()(x)
+
+    top_dropout_rate = 0.2
+    x = tf.keras.layers.Dropout(top_dropout_rate, name="top_dropout")(x)
+
+    output = tf.keras.layers.Dense(units=NUM_CLASSES, activation='softmax')(x)
+
+    model = tf.keras.Model(inputs=efficientNet.input, outputs=output)
+
+    # Freeze all the layers before the target layer
+    '''
+    for layer in model.layers[:200]:
+      layer.trainable =  False
+    '''
+
+    for layer in model.layers:
+      layer.trainable =  False
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    #print("Number of layers in modified model: ", len(model.layers))
+    #print(len(model.trainable_variables))
+    #print(model.summary())
+    return model
 
 
 
@@ -91,7 +143,16 @@ def plot_confusion_matrix(y_true,y_pred):
     plt.rcParams["figure.figsize"] = fig_size    
     
     
-    
+def finetune_model(model):
+    # Unfreeze the top 20 layers, and leave BatchNorm layers frozen
+    for layer in model.layers[-20:]:
+        if not isinstance(layer, tf.keras.layers.BatchNormalization):
+            layer.trainable = True
+
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
+
+   
     
     
     
@@ -107,19 +168,102 @@ valid_dir = os.path.join('assets/valid') #change to ur own directory.
 train_generator, validation_generator, test_generator = image_gen_w_aug(train_dir, test_dir, valid_dir)
 
 
-base_model = efn.EfficientNetB0(input_shape = (224, 224, 3), include_top = False, weights = 'imagenet')
 
-for layer in base_model.layers:
-    layer.trainable = False
-    
-last_layer = base_model.get_layer('mixed3')
-last_output = last_layer.output  #last layer is output.    
-    
-model_TL = model_output_for_TL(base_model, last_output)
-    
-model_TL.compile(tf.keras.optimizers.RMSprop(lr=0.0001, decay=1e-6),loss='categorical_crossentropy',metrics=['accuracy'])    
-    
-eff_history = model_TL.fit_generator(train_generator, validation_data = validation_generator, steps_per_epoch = 100, epochs = 10)
+
+
+
+
+
+
+model = create_model()
+
+start_time = time.time()
+
+hist1 = model.fit_generator(train_generator, validation_data = validation_generator, steps_per_epoch = 10, epochs = 10)
+
+
+end_time = time.time()
+
+#model.save("/content/drive/My Drive/CS_231A/saved_models/" + MODEL_NAME)
+
+print("Total training time: {} seconds".format(end_time - start_time))
+#print(model.summary())
+plot_hist(hist1)
+
+
+tf.keras.models.save_model(model,'CN345_CIVIA_E4.hdf5') #will be save as this file
+
+
+
+
+
+
+
+# EFFICIENT NET BEE ZERO
+#import efficientnet.keras as efn
+#model = efn.EfficientNetB0(input_shape = (224, 224, 3), include_top = False, weights = 'imagenet')
+#for layer in model.layers:
+#    layer.trainable = False
+#x = model.output
+#x = Flatten()(x)
+#x = Dense(1024, activation="relu")(x)
+#x = Dropout(0.5)(x)
+#predictions = Dense(10, activation="softmax")(x)
+#model_final = Model(input = model.input, output = predictions)  
+#model_final.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])   
+#eff_history = model.fit_generator(train_generator, validation_data = validation_generator, steps_per_epoch = 12, epochs = 10)
+#plot_hist(eff_history) # PLOT Histogram
+#tf.keras.models.save_model(model,'CN345_CIVIA.hdf5') #will be save as this file
+
+
+
+#VGG-16
+
+#from tensorflow.keras.applications.vgg16 import VGG16
+#base_model = VGG16(input_shape = (224, 224, 3), # Shape of our images
+#include_top = False, # Leave out the last fully connected layer
+#weights = 'imagenet')
+#for layer in base_model.layers:
+#    layer.trainable = False
+#x = Flatten()(base_model.output)
+# Add a fully connected layer with 512 hidden units and ReLU activation
+#x = Dense(512, activation='relu')(x)
+# Add a dropout rate of 0.5
+#x = Dropout(0.2)(x)
+# Add a final sigmoid layer for classification
+#x = Dense(10, activation='softmax')(x)
+#model = tf.keras.models.Model(base_model.input, x)  
+#model.compile(optimizer = 'adam', loss = 'categorical_crossentropy',metrics = ['acc'])
+#vgghist = model.fit(train_generator, validation_data = validation_generator, steps_per_epoch = 100, epochs = 10)
+#plot_hist(vgghist) # PLOT Histogram
+#tf.keras.models.save_model(model,'CN345_CIVIA-VGG16.hdf5') #will be save as this file
+
+
+
+#RESNET50
+
+#from tensorflow.keras.applications import ResNet50
+#from tensorflow.python.keras.models import Sequential
+#from tensorflow.python.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+#base_model = Sequential()
+#base_model.add(ResNet50(include_top=False, weights='imagenet', pooling='max'))
+#base_model.add(Dense(10, activation='softmax'))
+#base_model.compile(optimizer = tf.keras.optimizers.SGD(lr=0.0001), loss = 'categorical_crossentropy', metrics = ['acc'])
+#resnet_history = base_model.fit(train_generator, validation_data = validation_generator, steps_per_epoch = 12, epochs = 10)
+#plot_hist(resnet_history)
+#tf.keras.models.save_model(base_model,'CN345_CIVIA-VGG16.hdf5')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -157,4 +301,3 @@ plot_hist(history_TL)
 
 
 tf.keras.models.save_model(model_TL,'CN345_CIVIA.hdf5') #will be save as this file
-
